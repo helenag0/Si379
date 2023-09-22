a1ab5e2876c15ef7772ff78d4e2cd63d622a0116
hgrobel <hgrobel@umich.edu>
2023-09-22 14:57:32 -0400

    finished p2

---
+++ ps2/si_379_2/si_379_2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  si_379_2
+//
+//  Created by Helena Grobel on 9/22/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
