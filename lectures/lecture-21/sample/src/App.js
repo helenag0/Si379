2da81ac6a2b43975bb42242e85fb3eba48b85c3d
hgrobel <hgrobel@umich.edu>
2023-11-09 10:59:47 -0500

    updates

---
+++ lectures/lecture-21/sample/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React from "react";
+
+function ToDoList(props) {
+  return <div>
+  <ul></ul>
+  <input type="text" />
+  <button>Add</button>
+</div>
+}
+
+export default ToDoList;
